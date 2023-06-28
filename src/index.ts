bcba6051221fe3e1863b2de29fe1effcac964982
Aashan Ghimire <ashanghimire10@gmail.com>
2023-06-29 02:43:15 +0545

    Added input fields to demonstrate use of signals

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import {createSignal} from '../lib';
+
+
+const [firstName, setFirstName] = createSignal("John");
+const [lastName, setLastName] = createSignal("Doe");
+const [showFullName, setShowFullName] = createSignal(false);
+
+const fullName = () => {
+    return `${firstName()} ${lastName()}`;
+}
+
+const firstNameInput = document.createElement("input");
+firstNameInput.value = firstName();
+const lastNameInput = document.createElement("input");
+lastNameInput.value = lastName();
+const showFullNameInput = document.createElement("input");
+showFullNameInput.type = "checkbox";
+showFullNameInput.checked = showFullName();
+
+
+document.body.appendChild(firstNameInput);
+document.body.appendChild(lastNameInput);
+document.body.appendChild(showFullNameInput);
+
+firstNameInput.addEventListener("input", (e) => {
+    setFirstName((e.target as HTMLInputElement).value);
+});
+
+lastNameInput.addEventListener("input", (e) => {
+    setLastName((e.target as HTMLInputElement).value);
+});
+
+showFullNameInput.addEventListener("change", (e) => {
+    setShowFullName((e.target as HTMLInputElement).checked);
+});
+
+const fullNameDisplay = document.createElement("h2");
+document.body.appendChild(fullNameDisplay);
+
+
+
